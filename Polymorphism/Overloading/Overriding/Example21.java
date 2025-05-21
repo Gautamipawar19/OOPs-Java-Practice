b0843d90e3af7ef64d8cc74a8569870e20630b7b
Gautamipawar19 <gautamipawar19@gmail.com>
2025-05-21 23:35:22 +0530

    Is it possible to override main method?
    Yes, Is it possible to override main method.

---
+++ Polymorphism/Overloading/Overriding/Example21.java
@@ -0,0 +1,17 @@
+class Example21
+{
+	public static void main(int a)
+	{
+		System.out.println("Inside main - int ");
+	}								    
+	public static void main(boolean b)
+	{
+		System.out.println("Inside main - Boolean");
+	}
+	public static void main(String args[])
+	{
+		System.out.println("Start @ Cyber Success");
+		main(100);
+		System.out.println("Stop @ Cyber Success");
+	}
+}
