9d1729e3b34c50848bf3ce9c1e6faa1f4d6e24b9
Gautamipawar19 <gautamipawar19@gmail.com>
2025-05-21 23:03:59 +0530

    Overriding Rule 7-A
    It is possible to override parent and child method access level is the same.

---
+++ Polymorphism/Overloading/Overriding/Example17.java
@@ -0,0 +1,24 @@
+class Sample
+{
+	public void m1()
+	{
+		System.out.println("Inside m1 - Sample");
+	}
+}
+class Demo extends Sample
+{
+	public void m1()
+	{
+		System.out.println("Inside m1 - Demo");
+	}
+}
+class Example17
+{
+	public static void main(String args[])
+	{
+		System.out.println("Start @ Cyber Success");
+		Sample ref = new Demo();
+		ref.m1();
+		System.out.println("Stop @ Cyber Success");
+	}
+}
