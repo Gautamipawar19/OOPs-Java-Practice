6ad51e748005eb07d40130f9629af5bbf955a0ba
Gautamipawar19 <gautamipawar19@gmail.com>
2025-05-27 00:09:24 +0530

    Abstraction
    In abstract we can also create a variables

---
+++ Abstraction/Abstract class/Example13.java
@@ -0,0 +1,14 @@
+abstract class Demo
+{
+	int a=10;
+}
+class Example13	 extends Demo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Start @ Cyber Success");
+		Example13 ref = new Example13();
+		System.out.println(ref.a);
+		System.out.println("Stop @ Cyber Success");
+	}
+}
