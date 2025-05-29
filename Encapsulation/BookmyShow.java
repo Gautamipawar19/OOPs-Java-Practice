54b6b59a5836f33770980e78cb97ee1beccd01e1
Gautamipawar19 <gautamipawar19@gmail.com>
2025-05-29 23:47:07 +0530

    Encapsulation
    Singleton Design Patterns.

---
+++ Encapsulation/BookmyShow.java
@@ -0,0 +1,29 @@
+class Pvrmovies
+{
+	private static Pvrmovies mticket;
+	private static boolean ticketstatus = false;
+	private Pvrmovies()
+	{
+		System.out.println("Welcome to Pvrmovies Entertinment.....");
+	}
+	public static Pvrmovies bookTicket()
+	{
+		System.out.println("please wait your ticket booking is in progress");
+		if(ticketstatus == false)
+		{							  
+			mticket = new Pvrmovies();
+			ticketstatus = true;
+		}
+		return mticket;
+	}
+}
+class BookmyShow
+{
+	public static void main(String args[])
+	{
+		Pvrmovies custTicket1 = Pvrmovies.bookTicket();
+		System.out.println(custTicket1);//old address
+		custTicket1 = Pvrmovies.bookTicket();
+		System.out.println(custTicket1);//new address //old address
+   	}
+}
