4b011d1fcb8ddb6aa596f1258d7d066313adc6ee
Gautamipawar19 <gautamipawar19@gmail.com>
2025-07-21 21:19:11 +0530

    Nested Classes

---
+++ Inner Class & Nested Class/Example14.java
@@ -0,0 +1,15 @@
+class Demo
+{
+	static class Sample
+	{
+		static int a=100;
+	}
+		
+}
+class Example14
+{
+	public static void main(String args[])
+	{
+		System.out.println(Demo.Sample.a);
+	}
+}
